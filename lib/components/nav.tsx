025ccaaad192b46a68911b15d476ef93f41b0b30
Ibrahim Mammadov <zxyctn@gmail.com>
2024-08-18 13:15:07 +0200

    Update navbar content

--- lib/components/nav.tsx
+++ lib/components/nav.tsx
@@ -1,24 +1,18 @@
 'use client';
 
-import { SignIn } from '@phosphor-icons/react';
-import ThemeToggler from './theme-toggler';
+import Link from 'next/link';
+
+import ThemeToggler from '@/lib/components/theme-toggler';
 
 const Nav = () => {
   return (
     <div className='flex items-center justify-between'>
-      <div className=''>
-        <a href='/' className=''>
-          <span className='font-bold text-6xl text-primary dark:text-primary-dark'>
-            _blog
-          </span>
-        </a>
-      </div>
-      <div className='flex items-center gap-8'>
-        <ThemeToggler />
-        <a href='/login' className=''>
-          <SignIn size={32} className='text-primary' />
-        </a>
-      </div>
+      <Link href='/'>
+        <span className='font-bold text-4xl md:text-5xl lg:text-6xl text-primary dark:text-primary-dark'>
+          _blog
+        </span>
+      </Link>
+      <ThemeToggler />
     </div>
   );
 };
